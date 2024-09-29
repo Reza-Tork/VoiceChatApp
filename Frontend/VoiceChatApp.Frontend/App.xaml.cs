eb712408634cf75562fff8092740769c0109e2db
TheDevPlus <firstprogrammer0@gmail.com>
2024-09-29 12:04:44 +0330

    Add project files.

---
+++ Frontend/VoiceChatApp.Frontend/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VoiceChatApp.Frontend;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Frontend/VoiceChatApp.Frontend/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace VoiceChatApp.Frontend;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
